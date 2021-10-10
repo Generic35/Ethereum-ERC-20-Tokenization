eda82e5bda06966354fdc632b5105b26f2349883
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-10 15:10:15 -0400

    feat: Add KYC and tests

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,5 +1,6 @@
 const TokenSale = artifacts.require('MyTokenSale');
 const Token = artifacts.require('MyToken');
+const KycContract = artifacts.require('KycContract');
 
 const chai = require('./setupchai.js');
 const BN = web3.utils.BN;
@@ -30,15 +31,22 @@ contract('TokenSale test', async (accounts) => {
   it('should be possible to buy tokens', async () => {
     let tokenInstance = await Token.deployed();
     let tokenSaleInstance = await TokenSale.deployed();
+    let kycInstance = await KycContract.deployed();
     let balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+
+    await kycInstance.setKycCompleted(deployerAccount, {
+      from: deployerAccount,
+    });
+
     expect(
       tokenSaleInstance.sendTransaction({
         from: deployerAccount,
         value: web3.utils.toWei('1', 'wei'),
       })
     ).to.be.fulfilled;
+    balanceBefore = balanceBefore.add(new BN(1));
     return expect(
       tokenInstance.balanceOf(deployerAccount)
-    ).to.eventually.be.a.bignumber.equal(new BN(1));
+    ).to.eventually.be.a.bignumber.equal(balanceBefore);
   });
 });
