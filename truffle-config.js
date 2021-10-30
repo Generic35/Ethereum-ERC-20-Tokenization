b6666f61c17138cfcafec67af6fdc1de6e42ef75
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-30 17:02:23 -0400

    feat: Setup truffle HD wallet 2

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -10,6 +13,12 @@ module.exports = {
       network_id: 5777,
       host: '127.0.0.1',
     },
+		ganache_local: {
+			provider: function() {
+				return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex);
+			},
+			network_id: 5777
+		}
   },
   compilers: {
     solc: {
